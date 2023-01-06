4b385fbcaee710b0638ebb2d98aa0ff6c5718407
IMcQueenI <m.gorelenko@mail.ru>
2023-01-06 14:20:23 +0300

    fix: files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    let zero = ['zero']
+    let oneToNine = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let twentyToNinety = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundred = ['one hundred'];
+    if (n === 0){
+        return zero[0];
+    }
+    else if (n < 10){
+        return oneToNine[n];
+    }
+    else if ((n >= 10) && (n < 20)){
+        return tenToNineteen[n%10];
+    }
+    else if ((n >= 20) && (n < 100)){
+        return (twentyToNinety[Math.floor(n/10)] + ' ' + oneToNine[n%10]).trim();
+    }
+    else if (n === 100){
+        return hundred[0];
+    }
+    else if (n >= 100 && n < 1000){
+        if ((n%100 === 0)){
+            return (oneToNine[Math.floor(n/100)] + ' hundred');
+        }else if (n%100 < 10 && n%100 > 0 ){
+            return (oneToNine[Math.floor(n/100)] + ' hundred ' + oneToNine[n%100]);
+        }else if (n%100 < 20 && 9 < n%100){
+            return (oneToNine[Math.floor(n/100)] + ' hundred ' + tenToNineteen[n%10]);
+        }else if (n%10 === 0 && n%100 !== 0){
+            return (oneToNine[Math.floor(n/100)] + ' hundred ' + twentyToNinety[((n%100)/10)]);
+        }else if (n%100 >= 20 && n%10 !== 0){
+            return (oneToNine[Math.floor(n/100)] + ' hundred ' + twentyToNinety[Math.floor((n%100)/10)] + ' ' + oneToNine[n%10]);
+        }
+    }
 }
